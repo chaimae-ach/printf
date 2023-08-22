4fa3c6588062afcc5fbdd012416ae56676ebdd95
chaimae-ach <chaimae18achchar@gmail.com>
2023-08-21 18:50:08 -0700

    task0

--- main.h
+++ main.h
@@ -1,10 +1,12 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-
+functions 
+int _printf(const char *format, ...);
+int _putchar(char);
 
 #endif
